2e26b4dd1ec88031c9fba372dd7f087fe367990e
mayrabevacqua <mayrabevacqua@gmail.com>
2024-11-01 19:12:26 -0300

    Creé clase lavarropa

---
+++ lavarropa.ts
@@ -0,0 +1,73 @@
+import { Electrodomestico } from "./electrodomesticos";
+
+export class Lavarropa extends Electrodomestico {
+    private capacidad: number;
+    private consumoElectrico: string;
+    private tipoCarga: string;
+
+
+    constructor(capacidad: number, consumoElectrico: string, tipoCarga: string) {
+        super();
+        this.capacidad = capacidad;
+        this.consumoElectrico = consumoElectrico;
+        this.tipoCarga = tipoCarga;
+    }
+
+    // Getters
+    public getCapacidad(): number {
+        return this.capacidad;
+    }
+
+    public getConsumoElectrico(): string {
+        return this.consumoElectrico;
+    }
+
+    public getTipoCarga(): string {
+        return this.tipoCarga;
+    }
+
+    // Setters
+    public setCapacidad(capacidad: number): void {
+        this.capacidad = capacidad;
+    }
+
+    public setConsumoElectrico(consumoElectrico: string): void {
+        this.consumoElectrico = consumoElectrico;
+    }
+
+    public setTipoCarga(tipoCarga: string): void {
+        this.tipoCarga = tipoCarga;
+    }
+
+    // Centrifugar
+    public centrifugar(): void {
+        console.log("Centrifugando...");
+    }
+
+    // Enjuagar
+    public enjuagar(): void {
+        console.log("Enjuagando...");
+    }
+
+    // Prelavado
+    public prelavado(): void {
+        console.log("Realizando prelavado...");
+    }
+
+    // Lavado
+    public lavado(): void {
+        console.log("Lavando...");
+    }
+
+    // Métodos de clase padre (heredados)
+    public prender(): void {
+        super.prender();
+        console.log("Lavarropa encendido.");
+    }
+
+    public apagar(): void {
+        super.apagar();
+        console.log("Lavarropa apagado.");
+    }
+
+}
